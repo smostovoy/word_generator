59ff5ea5688fa480438c13d9fd64c9034edb6a27
sergey_mo <svmostovoy@gmail.com>
2012-09-16 10:47:03 +0300

    fix_spelling

--- generate.rb
+++ generate.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 search_pattern = ARGV[0]
-search_liters = search_pattern.split ''
+search_letters = search_pattern.split ''
 rules = {
     all_present: true,
     end_with: true
@@ -11,9 +11,9 @@ files.each do |file|
   File.open file do |infile|
     while (line = infile.gets)
       dic_word = line.split(/[\s\/]/).first
-      #p "/^#{"[#{search_pattern}]" * search_liters.size} $/"
+      #p "/^#{"[#{search_pattern}]" * search_letters.size} $/"
       if rules[:all_present]
-        p dic_word if dic_word =~ /^[#{search_pattern}]{#{search_liters.size}}$/
+        p dic_word if dic_word =~ /^[#{search_pattern}]{#{search_letters.size}}$/
       end
       if rules[:end_with]
         p dic_word if dic_word =~ /#{search_pattern}$/
